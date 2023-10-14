542592fe0741f2ead538e6657357b142bdf2149a
Pavlo <pavelkortp@gmail.com>
2023-10-15 02:00:27 +0300

    completes last task

--- com/shpp/pzhurbytskyi/Express/myapp/index.js
+++ com/shpp/pzhurbytskyi/Express/myapp/index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    let counter = require('fs').readFileSync('com/shpp/pzhurbytskyi/Express/myapp/count.txt', 'utf-8');
+	res.send((counter++)+'');
+    require('fs').writeFileSync('com/shpp/pzhurbytskyi/Express/myapp/count.txt', (counter)+'');
+});
+
+app.post('/sum', (req, res) => {
+	const params = req.query;
+    res.send(((+params.a)+(+params.b))+'');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`)
+});
